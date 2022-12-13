535ddbbbc86b8b1abe44c6951af7acb3743ebf69
Jakubik, Peter (MO RC-SK MM-COC E2 5) <peter.jakubik@siemens.com>
2022-12-13 17:39:08 +0100

    Bkp

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,28 @@
 package com.example.demo;
 
+import io.micrometer.common.KeyValue;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.annotation.Observed;
+import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpHeaders;
 import java.util.Random;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,6 +31,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+		return new ObservedAspect(observationRegistry);
+	}
 }
 
 @RestController
@@ -35,7 +50,7 @@ class MyController {
 	}
 
 	@GetMapping("/user/{userId}")
-	String userName(@PathVariable("userId") String userId) {
+	public String userName(HttpServletRequest request, @PathVariable("userId") String userId) {
 		log.info("Got a request");
 
 		meterRegistry.counter("users.total", "userid", userId).increment();
@@ -69,5 +84,34 @@ class MyUserService {
 			throw new RuntimeException(e);
 		}
 		return "foo";
+	}
+}
+
+@Component
+class MyHandler implements ObservationHandler<Observation.Context> {
+
+	private static final Logger log = LoggerFactory.getLogger(MyHandler.class);
+
+	@Override
+	public void onStart(Observation.Context context) {
+		log.info("Before running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
+	}
+
+	@Override
+	public void onStop(Observation.Context context) {
+		log.info("After running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
+	}
+
+	@Override
+	public boolean supportsContext(Observation.Context context) {
+		return true;
+	}
+
+	private String getUserTypeFromContext(Observation.Context context) {
+		return StreamSupport.stream(context.getLowCardinalityKeyValues().spliterator(), false)
+				.filter(keyValue -> "userType".equals(keyValue.getKey()))
+				.map(KeyValue::getValue)
+				.findFirst()
+				.orElse("UNKNOWN");
 	}
 }
